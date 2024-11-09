a97f54e4025d4e0cf115ce039e3cab9d7cda82d9
Ricomaldo <rico2maldo@gmail.com>
2024-11-09 15:03:03 +0100

    test

---
+++ FrontEnd/js/main.js
@@ -0,0 +1,8 @@
+// main.js
+
+// Importation des modules
+import { afficherTravaux } from "./works.js";
+
+// Génération de l'affichage des travaux
+afficherTravaux();
+console.log("La galerie a été générée, bravo !");
